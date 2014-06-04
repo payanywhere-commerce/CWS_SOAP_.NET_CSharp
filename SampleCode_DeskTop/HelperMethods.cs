f4eb8eb85e93fd32accfa9ab987aeb38f3bf0529
Zeb Bowles <zbowles@nabancard.com>
2014-06-04 13:38:06 -0600

    Changed license/disclaimer info

--- SampleCode_DeskTop/HelperMethods.cs
+++ SampleCode_DeskTop/HelperMethods.cs
@@ -1,5 +1,5 @@
 #region DISCLAIMER
-/* Copyright (c) 2014 PayAnywhere Commerce, LLC. - All Rights Reserved.
+/* Copyright (c) 2014 NAB, LLC. - All Rights Reserved.
  *
  * This software and documentation is subject to and made
  * available only pursuant to the terms of an executed license
@@ -7,7 +7,7 @@
  * of said agreement. This software may not, in whole or in part,
  * be copied, photocopied, reproduced, translated, or reduced to
  * any electronic medium or machine-readable form without
- * prior consent, in writing, from PayAnywhere Commerce, LLC.
+ * prior consent, in writing, from NAB, LLC.
  *
  * Use, duplication or disclosure by the U.S. Government is subject
  * to restrictions set forth in an executed license agreement
@@ -19,7 +19,7 @@
  * 16-52.227-86; or their equivalent.
  *
  * Information in this software is subject to change without notice
- * and does not represent a commitment on the part of IP Commerce.
+ * and does not represent a commitment on the part of NAB.
  * 
  * Sample Code is for reference Only and is intended to be used for educational purposes. It's the responsibility of 
  * the software company to properly integrate into thier solution code that best meets thier production needs. 
@@ -1963,7 +1963,8 @@ namespace SampleCode
             {
                 String clearToken = Encoding.UTF8.GetString(Convert.FromBase64String(identityToken));
 
-                //Now try and retrieve the Service Key from the XML
+                //Now try and retrieve the Service Key from the 
+
                 XmlDocument doc = new XmlDocument();
                 doc.LoadXml(clearToken);
                 XPathNavigator xnav = doc.CreateNavigator();
